ef763f646da5d8042b37f1786738809e8d89a9dc
Kyle Bock <kylewbock@gmail.com>
2011-10-18 12:17:41 -0400

    project setup

---
+++ KBRedditEngine/KBRedditUser.h
@@ -0,0 +1,13 @@
+//
+//  KBRedditUser.h
+//  KBRedditEngine
+//
+//  Created by Kyle Bock on 10/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KBRedditUser : NSObject
+
+@end
---
+++ KBRedditEngine/KBRedditUser.m
@@ -0,0 +1,23 @@
+//
+//  KBRedditUser.m
+//  KBRedditEngine
+//
+//  Created by Kyle Bock on 10/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "KBRedditUser.h"
+
+@implementation KBRedditUser
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
