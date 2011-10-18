e2a9b9598d5f8fc1502c93af4081f509aa757a30
Kyle Bock <kylewbock@gmail.com>
2011-10-18 11:58:06 -0400

    Initial Commit

---
+++ KBRedditEngine/KBRedditEngine.m
@@ -0,0 +1,23 @@
+//
+//  KBRedditEngine.m
+//  KBRedditEngine
+//
+//  Created by Kyle Bock on 10/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "KBRedditEngine.h"
+
+@implementation KBRedditEngine
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
